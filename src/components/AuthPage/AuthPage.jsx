c2b99c329d8143571c7a7b132a5a97d9b532d411
Igor Kolpakov <gragass00@yandex.ru>
2022-12-12 14:57:22 +0300

    fix: auth pages layout

--- src/components/AuthPage/AuthPage.jsx
+++ src/components/AuthPage/AuthPage.jsx
@@ -17,20 +17,22 @@ function AuthPage({ content, children }) {
         <h1 className='auth__title'>{title}</h1>
         <form className='auth__form' onSubmit={handleSubmit}>
           <fieldset className='auth__fieldset'>{children}</fieldset>
-          <div className='auth__button-wrapper'>
-            <span className='auth__error auth__error_active'>Ошибка!</span>
-            <button
-              className='auth__button-submit auth__button-submit_disable'
-              type='submit'
-            >
-              {buttonText}
-            </button>
-          </div>
-          <div className='auth__caption-wrapper'>
-            <p className='auth__caption-text'>{captionText}</p>
-            <Link className='auth__link' to={path}>
-              {linkText}
-            </Link>
+          <div className='auth__button-section-wrapper'>
+            <div className='auth__button-wrapper'>
+              <span className='auth__error auth__error_active'>Ошибка!</span>
+              <button
+                className='auth__button-submit auth__button-submit_disable'
+                type='submit'
+              >
+                {buttonText}
+              </button>
+            </div>
+            <div className='auth__caption-wrapper'>
+              <p className='auth__caption-text'>{captionText}</p>
+              <Link className='auth__link' to={path}>
+                {linkText}
+              </Link>
+            </div>
           </div>
         </form>
       </div>
