9648a9078501b7ef7392b745e914029a9ea9affb
Igor Kolpakov <gragass00@yandex.ru>
2023-02-22 10:37:35 +0300

    fix footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,31 +2,19 @@ import React from 'react'
 import './Footer.scss'
 function Footer() {
   return (
-    <footer className='footer'>
-      <div className='footer__inner'>
-        <p className='footer__caption'>
-          Учебный проект Яндекс.Практикум х BeatFilm.
-        </p>
-        <div className='footer__wrapper'>
-          <p className='footer__date'>&#xa9; 2022</p>
-          <nav className='footer__links'>
-            <ul className='footer__links-list'>
-              <li className='footer__link-item'>
+    <footer className="footer">
+      <div className="footer__inner">
+        <p className="footer__caption">kip0 х BeatFilm.</p>
+        <div className="footer__wrapper">
+          <p className="footer__date">&#xa9; 2022</p>
+          <nav className="footer__links">
+            <ul className="footer__links-list">
+              <li className="footer__link-item">
                 <a
-                  href='https://praktikum.yandex.ru/'
-                  target='_blank'
-                  className='footer__link'
-                  rel='noreferrer'
-                >
-                  Яндекс.Практикум
-                </a>
-              </li>
-              <li className='footer__link-item'>
-                <a
-                  href='https://github.com/Kip-ochka'
-                  target='_blank'
-                  className='footer__link'
-                  rel='noreferrer'
+                  href="https://github.com/Kip-ochka"
+                  target="_blank"
+                  className="footer__link"
+                  rel="noreferrer"
                 >
                   GitHub
                 </a>
